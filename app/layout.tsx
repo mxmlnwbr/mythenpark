607df9f94a382996385c034073b07666e9de48a8
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-03-11 18:23:36 +0100

    add: foter

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar";
+import Footer from "@/components/ui/footer";
 // Ensuring TypeScript recognizes the navbar component
 
 const geistSans = Geist({
@@ -27,12 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen grid grid-rows-[auto_1fr]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen grid grid-rows-[auto_1fr_auto]`}
       >
         <Navbar />
         <main>
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
